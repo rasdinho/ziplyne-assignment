82cce7c37f02b451e17dc6886179c79167057121
rasdinho <ras.dinho7@gmail.com>
2020-09-23 14:53:14 -0400

    ziplyne-latest-update

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div id="main">
+                <div id="bodyy">
+                    <div class="ocean">
+                    <div class="wave"></div>
+                    <div id="wave-content">
+                        <h1>Lorem Ipsum Dolor Lorem Ipsum Dolor Amet.</h1>
+                    </div>
+                    <div class="wave"></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
--- src/components/ProductSection.js
+++ src/components/ProductSection.js
@@ -4,23 +4,23 @@ class ProductSection extends Component {
     render() {
         return (
             <div>
-                <section id="product">
-                    <div data-aos="fade-in"className="section-title">
-                        <h1>Lorem Ipsum Dolor Lorem Ipsum Dolor Amet.</h1>
+                <section id="secondpage">
+                    <div data-aos="fade-in" className="second-title">
+                        <h1 id="firstpage-titile">Lorem Ipsum Dolor Lorem Ipsum Dolor Amet.</h1>
                     </div>
-                    <div id="product-details">
+                    <div id="secondpage-details">
                         <div id="product-img" data-aos="flip-left">
                             <img src={require('../images/Untitled design (15).png')} alt="product"/>
                         </div>
                         <div id="about-product">
                             <div id="diamond" data-aos="zoom-in">
                                 <img src={require("../images/Diamond.png")} alt="."/>
-                                <p data-aos="fade-in">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, incididunt ut labore et dolore magna aliqua. </p>
+                                <p data-aos="fade-in">Ziplyne enables you, your customers and employees to navigate the hills and valleys of The SaaS Terrain. ... Ziplyne is an Enterprise SaaS technology Company Focused On Simplifying User Experiences For All Web Appplications And Websites.</p>
                             </div>
                             
                         </div>
                     </div>
-                    <div className="banner">
+                    <div className="lastpart">
 
                     </div>
                 </section>
