b16f5ed4b86891ef10a586bd275e4d2b8f9f65c1
rasdinho <ras.dinho7@gmail.com>
2020-09-23 19:20:57 -0400

    add-more-animation

--- src/components/ProductSection.js
+++ src/components/ProductSection.js
@@ -5,17 +5,24 @@ class ProductSection extends Component {
         return (
             <div>
                 <section id="secondpage">
-                    <div data-aos="fade-in" className="second-title">
+                    <div className="second-title">
                         <h1 id="firstpage-titile">Lorem Ipsum Dolor Lorem Ipsum Dolor Amet.</h1>
                     </div>
                     <div id="secondpage-details">
-                        <div id="product-img" data-aos="flip-left">
+                        <div id="product-img">
                             <img src={require('../images/Untitled design (15).png')} alt="product"/>
                         </div>
                         <div id="about-product">
-                            <div id="diamond" data-aos="zoom-in">
+                            <div id="diamond">
                                 <img src={require("../images/Diamond.png")} alt="."/>
-                                <p data-aos="fade-in">Ziplyne enables you, your customers and employees to navigate the hills and valleys of The SaaS Terrain. ... Ziplyne is an Enterprise SaaS technology Company Focused On Simplifying User Experiences For All Web Appplications And Websites.</p>
+                                
+                                <p> <em id='ziplyne-word'>  <span id="ziplyne-span">Z</span>
+                                                            <span id="ziplyne-span">i</span>
+                                                            <span id="ziplyne-span">p</span>
+                                                            <span id="ziplyne-span">l</span>
+                                                            <span id="ziplyne-span">y</span>
+                                                            <span id="ziplyne-span">n</span>
+                                                            <span id="ziplyne-span">e</span></em> <br/>enables you, your customers and employees to navigate the hills and valleys of The SaaS Terrain. ... Ziplyne is an Enterprise SaaS technology Company Focused On Simplifying User Experiences For All Web Appplications And Websites.</p>
                             </div>
                             
                         </div>
