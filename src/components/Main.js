8b4b7f284037abec8d18a045a52b0495f1a0eafd
rasdinho <ras.dinho7@gmail.com>
2020-09-23 22:42:20 -0400

    -final-version-ziplyne

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,26 +4,14 @@ class Main extends Component {
     render() {
         return (
             <div id="main">
-                {/* <div id="bodyy">
-                    <div class="ocean">
-                    <div class="wave"></div>
-                    <div id="wave-content">
-                        <h1>Lorem Ipsum Dolor Lorem Ipsum Dolor Amet.</h1>
-                    </div>
-                    <div class="wave"></div>
-                    </div>
-                </div>  */}
                 <div id= "bodies">
                         <div id="pageWrapper">
                             <div class="wave">&nbsp;</div>
                             <div id="wave-content">
                                 <h1>Lorem Ipsum Dolor Lorem Ipsum Dolor Amet.</h1>
-                            </div>
-                                
-                        </div>
-                        
+                            </div>      
+                        </div> 
                 </div>
-             
             </div>
         );
     }
