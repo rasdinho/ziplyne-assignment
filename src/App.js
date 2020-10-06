1a2653caad4c46363a0ac9ae8fb057691a728d5f
rasdinho <ras.dinho7@gmail.com>
2020-10-06 16:09:24 -0400

    delete-extra-code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
-// import FirstImg from './images/ZLSampleDesk.png';
-// import SecondImg from './images/Untitled design (15).png';
-// import ThirdImg from './images/Diamond.png';
 import ProductSection from './components/ProductSection';
 
 
@@ -16,31 +13,6 @@ function App() {
       
       <HomePage />
       <ProductSection />
-      
-      <div id="firstHalf">
-          {/* <img src={FirstImg} alt='firstimg'/> */}
-      </div>
-      
-      <div>
-          <div>
-            {/* <h1>this is to try is out <br /> the second line</h1> */}
-              {/* <img src={SecondImg} alt='firstimg'/> */}
-          </div>
-
-          <div>
-              {/* <img src={SecondImg} alt='SecondImg'/> */}
-          </div>
-
-          <div>
-              {/* <img src={ThirdImg} alt='ThirdImg'/> */}
-          </div>
-
-          <div>
-              {/* <img src={FirstImg} alt='firstimg'/> */}
-          </div>
-
-
-      </div>
 
     </div>
   );
