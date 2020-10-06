430f26fbc6d6c316670fd82c69dcfaa270956c6f
rasdinho <ras.dinho7@gmail.com>
2020-10-06 15:52:16 -0400

    change-duration

--- src/components/Main.js
+++ src/components/Main.js
@@ -39,7 +39,7 @@ class Main extends Component {
 
                             <div id="wave">
 
-                                <div className="custom-shape-divider-bottom-1599186207">
+                                <div className="custom-shape-divider-bottom-1599186207" data-aos="fade-left">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1170 193">
                                         <path d="M1175 131.2c0 0-81-89.4-224.3-103.4S713 72 665 97c-86 46-148 63-271 7C221.7 25.5 56 104.5-4 197.4 -4 58.7-3.3 0.3-3.3 0.3L1175 0V131.2z"></path>
                                     </svg>
@@ -52,7 +52,7 @@ class Main extends Component {
 
                             </div>
 
-                            <div id="wave-content">
+                            <div id="wave-content" data-aos="fade-right">
                                 <h1 data-aos="fade-right">Lorem Ipsum Dolor Lorem Ipsum Dolor Amet.</h1>
                             </div>	
                 </div>
