7baed8293d882cbee3a9d6d7e7a16717022eec5d
rasdinho <ras.dinho7@gmail.com>
2020-10-28 16:58:28 -0400

    thirdpage-latest

--- src/components/ThirdPage.js
+++ src/components/ThirdPage.js
@@ -9,23 +9,23 @@ function ThirdPage() {
 
       <div id="bodyy3">
               <main>
-                <section className="background">
+                <section className="background3">
                 </section>
 
                       <section className="lp">
                               <div className="lp__nav">
-                                  <a href="." className="lp__nav__item"><img src="https://cdn.pixabay.com/photo/2014/04/03/00/40/bird-309064_1280.png" alt="."/></a>
+                                  <a href="." className="lp__nav__item"><img id="img3"src="https://cdn.pixabay.com/photo/2014/04/03/00/40/bird-309064_1280.png" alt="."/></a>
                                   <a href="." className="lp__nav__item"><i className="fab fa-facebook-f"></i></a>
                                   <a href="." className="lp__nav__item"><i className="fab fa-twitter"></i></a>
                                   <a href="." className="lp__nav__item"><i className="fab fa-vimeo-v"></i></a>
                               </div>
                         
                               <div className="lp__title">
-                                <h1>The <br></br> Symmetry</h1>
+                                <h1 id="page3">The <br></br> Ziplyne</h1>
                               </div>
                               
                               <div className="lp__cta">
-                                <a href=".">Subscribe</a>
+                                <a id="a3"href=".">Subscribe</a>
                               </div>
                         
                       </section>
