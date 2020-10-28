0075f39b8bb4b922285d1ef09d5c50f7c901ff0e
rasdinho <ras.dinho7@gmail.com>
2020-10-28 14:47:10 -0400

    add-footer-third-page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import ReactDOM from 'react'
 
-class Footer extends Component {
-    render() {
-        return (
-            <div>
-                <h1>should work now </h1>
-            </div>
-        );
-    }
+// class Footer extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>should work now </h1>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Footer;
+
+const img1 = "https://s3.eu-central-1.amazonaws.com/test.18feb.web.v2/18feb_v2_pic1.jpg";
+
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <div className="MainPage">
+        <div className="Header">
+          <div className="left-container">18feb</div>
+          <div className="right-container">
+            <span>New Collection</span>
+            <span>Women</span>
+            <span>Men</span>
+            <span>Kids</span>
+          </div>
+        </div>
+        <img src={img1} alt="testpic" />
+      </div>
+    );
+  }
 }
+ export default Footer;
 
-export default Footer;
+
+
