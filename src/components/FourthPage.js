09f979486ed8f469ead9dcabfcac4b56b89d68d6
rasdinho <ras.dinho7@gmail.com>
2020-11-05 16:47:05 -0500

    add-header-to-fourthPage

--- src/components/FourthPage.js
+++ src/components/FourthPage.js
@@ -3,9 +3,53 @@ import React, { Component } from 'react';
 class FourthPage extends Component {
     render() {
         return (
-            <div>
-                thids s the 4 test
+<>
+
+<header className="headerrr">
+        <div className="containerry">
+            <div className="toppy">
+                <div className="logo">
+                    <span>Lorem <br/>Ipsum</span>
+                </div>
+                <div className="sideMenu">
+                    <span className="caption">Lorem ipsum dolor.</span>
+                    <ul>
+                        <li><a href=".">lorem</a></li>
+                        <li><a href=".">ipsum</a></li>
+                        <li><a href=".">dolor</a></li>
+                    </ul>
+                </div>
+        </div>
+
+        <div className="line1">Lorem, ipsum dolor sit amet, consectetur.</div>
+            <div className="">
+                <p className="line2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime repudiandae est vero</p>
             </div>
+            <div className="btn">
+                <button>Lorem ipsum</button>
+            </div>
+        </div>
+    </header>
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</>
         );
     }
 }
