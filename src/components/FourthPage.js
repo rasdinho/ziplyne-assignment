1545e66b3755568bd22b555cff6f319fc6a9fb8b
rasdinho <ras.dinho7@gmail.com>
2020-11-05 18:56:38 -0500

    fourthpage-jsx-done

--- src/components/FourthPage.js
+++ src/components/FourthPage.js
@@ -1,60 +1,58 @@
 import React, { Component } from 'react';
+import './FourthPage.scss'
 
 class FourthPage extends Component {
     render() {
         return (
 <>
-
-<header className="headerrr">
-        <div className="containerry">
-            <div className="toppy">
-                <div className="logo">
+<section class="headerrr">
+        <div class="containerry">
+            <div class="toppy">
+                <div class="logooo">
                     <span>Lorem <br/>Ipsum</span>
                 </div>
-                <div className="sideMenu">
-                    <span className="caption">Lorem ipsum dolor.</span>
-                    <ul>
-                        <li><a href=".">lorem</a></li>
-                        <li><a href=".">ipsum</a></li>
-                        <li><a href=".">dolor</a></li>
+                <div class="sideMenuuu">
+                    <span class="captionnn">Lorem ipsum dolor.</span>
+                    <ul id="fourthUl">
+                        <li id="fourthli"><a id="fourthA" href=".">lorem</a></li>
+                        <li id="fourthli"><a id="fourthA" href=".">ipsum</a></li>
+                        <li id="fourthli"><a id="fourthA" href=".">dolor</a></li>
                     </ul>
                 </div>
-        </div>
-
-        <div className="line1">Lorem, ipsum dolor sit amet, consectetur.</div>
-            <div className="">
-                <p className="line2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime repudiandae est vero</p>
             </div>
-            <div className="btn">
-                <button>Lorem ipsum</button>
+            <div class="line111">Lorem, ipsum dolor sit amet, consectetur.</div>
+            <div class="">
+                <p class="line222">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime repudiandae est vero</p>
+            </div>
+            <div class="fourthbtn">
+                <button id="fourthbutton">Lorem ipsum</button>
             </div>
         </div>
-    </header>
-
+    </section>
     <section>
-        <div className="section1">
-            <div className="card">
+        <div class="section1-1">
+            <div class="card">
                 <h4>lorem</h4>
-                <div className="imgBox">
-                    <img href="." src="https://dribbble.s3.amazonaws.com/users/322/screenshots/872485/coldchase.jpg" alt=""/>
+                <div class="imgBox">
+                    <img href="." src="https://dribbble.s3.amazonaws.com/users/322/screenshots/872485/coldchase.jpg" alt="."/>
                 </div>
                 <h3>Lorem ipsum dolor</h3>
                 <p>Lorem ipsum, dolor sit amet consect adipisicing elit.
                      Aliquam blanditiis dolor eum minus, labore</p>
             </div>
-            <div className="card">
+            <div class="card">
                 <h4>lorem</h4>
-                <div className="imgBox">
-                    <img href="." src="https://dribbble.s3.amazonaws.com/users/322/screenshots/980517/icehut_sm.jpg" alt=""/>
+                <div class="imgBox">
+                    <img href="." src="https://dribbble.s3.amazonaws.com/users/322/screenshots/980517/icehut_sm.jpg" alt="."/>
                 </div>
                 <h3>Lorem ipsum dolor</h3>
                 <p>Lorem ipsum, dolor sit amet consect adipisicing elit.
                      Aliquam blanditiis dolor eum minus, labore</p>
             </div>
-            <div className="card">
+            <div class="card">
                 <h4>lorem</h4>
-                <div className="imgBox">
-                    <img href="." src="https://dribbble.s3.amazonaws.com/users/322/screenshots/943660/hq_sm.jpg" alt=""/>
+                <div class="imgBox">
+                    <img href="." src="https://dribbble.s3.amazonaws.com/users/322/screenshots/943660/hq_sm.jpg" alt="."/>
                 </div>
                 <h3>Lorem ipsum dolor</h3>
                 <p>Lorem ipsum, dolor sit amet consect adipisicing elit.
@@ -62,34 +60,20 @@ class FourthPage extends Component {
             </div>
         </div>
     </section>
-
     <footer>
-        <div className="footer">
-            <div className="copright">
+        <div class="footer">
+            <div class="copright">
                 <p>©Lorem ipsum dolor</p>
             </div>
-            <div className="footerNav">
-                <ul>
-                    <li><a href=".">lorem</a></li>
-                    <li><a href=".">ipsum</a></li>
-                    <li><a href=".">dolor</a></li>
+            <div class="footerNav">
+                <ul id="fourthUl">
+                    <li id="fourthli"><a id="fourthA" href=".">lorem</a></li>
+                    <li id="fourthli"><a id="fourthA" href=".">ipsum</a></li>
+                    <li id="fourthli"><a id="fourthA" href=".">dolor</a></li>
                 </ul>
             </div>
         </div>
-    </footer> 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    </footer>
 
 
 
