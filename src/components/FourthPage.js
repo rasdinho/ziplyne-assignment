4c924372abe71641c8a79e8a9d6f9c524ff57b51
rasdinho <ras.dinho7@gmail.com>
2020-11-05 19:01:49 -0500

    add-style-fourthpage-headerrr

--- src/components/FourthPage.js
+++ src/components/FourthPage.js
@@ -5,14 +5,14 @@ class FourthPage extends Component {
     render() {
         return (
 <>
-<section class="headerrr">
-        <div class="containerry">
-            <div class="toppy">
-                <div class="logooo">
+<section className="headerrr">
+        <div className="containerry">
+            <div className="toppy">
+                <div className="logooo">
                     <span>Lorem <br/>Ipsum</span>
                 </div>
-                <div class="sideMenuuu">
-                    <span class="captionnn">Lorem ipsum dolor.</span>
+                <div className="sideMenuuu">
+                    <span className="captionnn">Lorem ipsum dolor.</span>
                     <ul id="fourthUl">
                         <li id="fourthli"><a id="fourthA" href=".">lorem</a></li>
                         <li id="fourthli"><a id="fourthA" href=".">ipsum</a></li>
@@ -20,38 +20,38 @@ class FourthPage extends Component {
                     </ul>
                 </div>
             </div>
-            <div class="line111">Lorem, ipsum dolor sit amet, consectetur.</div>
-            <div class="">
-                <p class="line222">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime repudiandae est vero</p>
+            <div className="line111">Lorem, ipsum dolor sit amet, consectetur.</div>
+            <div className="">
+                <p className="line222">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime repudiandae est vero</p>
             </div>
-            <div class="fourthbtn">
+            <div className="fourthbtn">
                 <button id="fourthbutton">Lorem ipsum</button>
             </div>
         </div>
     </section>
     <section>
-        <div class="section1-1">
-            <div class="card">
+        <div className="section1-1">
+            <div className="card">
                 <h4>lorem</h4>
-                <div class="imgBox">
+                <div className="imgBox">
                     <img href="." src="https://dribbble.s3.amazonaws.com/users/322/screenshots/872485/coldchase.jpg" alt="."/>
                 </div>
                 <h3>Lorem ipsum dolor</h3>
                 <p>Lorem ipsum, dolor sit amet consect adipisicing elit.
                      Aliquam blanditiis dolor eum minus, labore</p>
             </div>
-            <div class="card">
+            <div className="card">
                 <h4>lorem</h4>
-                <div class="imgBox">
+                <div className="imgBox">
                     <img href="." src="https://dribbble.s3.amazonaws.com/users/322/screenshots/980517/icehut_sm.jpg" alt="."/>
                 </div>
                 <h3>Lorem ipsum dolor</h3>
                 <p>Lorem ipsum, dolor sit amet consect adipisicing elit.
                      Aliquam blanditiis dolor eum minus, labore</p>
             </div>
-            <div class="card">
+            <div className="card">
                 <h4>lorem</h4>
-                <div class="imgBox">
+                <div className="imgBox">
                     <img href="." src="https://dribbble.s3.amazonaws.com/users/322/screenshots/943660/hq_sm.jpg" alt="."/>
                 </div>
                 <h3>Lorem ipsum dolor</h3>
@@ -61,11 +61,11 @@ class FourthPage extends Component {
         </div>
     </section>
     <footer>
-        <div class="footer">
-            <div class="copright">
+        <div className="footer">
+            <div className="copright">
                 <p>©Lorem ipsum dolor</p>
             </div>
-            <div class="footerNav">
+            <div className="footerNav">
                 <ul id="fourthUl">
                     <li id="fourthli"><a id="fourthA" href=".">lorem</a></li>
                     <li id="fourthli"><a id="fourthA" href=".">ipsum</a></li>
