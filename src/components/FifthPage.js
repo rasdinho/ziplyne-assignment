0e6bf575e7bd627b89d4db7b1289bc1de9222f7b
rasdinho <ras.dinho7@gmail.com>
2020-11-20 22:08:01 -0500

    fifth-page-section-2

--- src/components/FifthPage.js
+++ src/components/FifthPage.js
@@ -4,23 +4,38 @@ import './FifthPage.scss'
 class FifthPage extends Component {
     render() {
         return (
-            <>
+<>
 
-    <section class="section-a-fifthpage">
-      <div class="fifthpage-container">
-        <div>
-          <h1 id="fourth-page-h1-4">The headphones of the future.</h1>
-          <p id="fifthpage-p">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-            repudiandae rerum libero ipsum asperiores omnis mollitia, nostrum
-            commodi placeat ea itaque modi corrupti corporis nam voluptas aut
-            reprehenderit eaque culpa.
-          </p>
+            <section class="section-a-fifthpage">
+                    <div class="fifthpage-container">
+                        <div>
+                                <h1 id="fourth-page-h1-4">The headphones of the future.</h1>
+                                <p id="fifthpage-p">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+                                    repudiandae rerum libero ipsum asperiores omnis mollitia, nostrum
+                                    commodi placeat ea itaque modi corrupti corporis nam voluptas aut
+                                    reprehenderit eaque culpa.
+                                </p>
+                        </div>
+                        <img src="https://i.ibb.co/37Y74kv/showcase.jpg" alt="." />
+                    </div>
+            </section>
+    {/*================================================================================================================*/}
+            <section id="about" class="section-b">
+                    <div class="overlay">
+                        <div class="section-b-inner py-5">
+                                <h3  id="fifthpage-h3" class="text-2">Loud & Clear</h3>
+                                <h2  id="fifthpage-h2" class="text-5 mt-1">People Aren't Hearing All the Music</h2>
+                                <p id="fifthpage-p2" class="mt-1">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+                                    repudiandae laboriosam quia, error tempore porro ducimus voluptate
+                                    laborum nostrum iure.
+                                </p>
+                        </div>
+                    </div>
+            </section>
+    {/*================================================================================================================*/}
 
-        </div>
-        <img src="https://i.ibb.co/37Y74kv/showcase.jpg" alt="." />
-      </div>
-    </section>
 
     
 
