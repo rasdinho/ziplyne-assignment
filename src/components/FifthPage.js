d2479d1205c112e322b184427e7146f87c08f57a
rasdinho <ras.dinho7@gmail.com>
2020-11-20 22:13:15 -0500

    fifth-page-section-3-pics-3extra-

--- src/components/FifthPage.js
+++ src/components/FifthPage.js
@@ -43,6 +43,12 @@ class FifthPage extends Component {
 
                         <a href="https://i.ibb.co/xSnHP7g/gal43884.jpg" class="big"><img src="https://i.ibb.co/xSnHP7g/gal43884.jpg" alt=""/></a>
 
+                        <a href="https://i.ibb.co/QN6Bnrb/gal4958.jpg" class="big"><img src="https://i.ibb.co/QN6Bnrb/gal4958.jpg" alt=""/></a>
+
+                        <a href="https://i.ibb.co/dGZvj75/gal4545.jpg" class="big"><img src="https://i.ibb.co/dGZvj75/gal4545.jpg" alt=""/></a>
+
+                        <a href="https://i.ibb.co/S6FVFNt/gal74744.jpg" class="big"><img src="https://i.ibb.co/S6FVFNt/gal74744.jpg" alt=""/></a>
+
                     </div>
             </section>
 
