384c38afc1aa1ff6d3c3f7989841ae97f7012571
rasdinho <ras.dinho7@gmail.com>
2020-11-20 22:01:22 -0500

    fifth-page-section-1

--- src/components/FifthPage.js
+++ src/components/FifthPage.js
@@ -4,9 +4,31 @@ import './FifthPage.scss'
 class FifthPage extends Component {
     render() {
         return (
-            <div>
-                this is a component number 5
-            </div>
+            <>
+
+    <section class="section-a-fifthpage">
+      <div class="fifthpage-container">
+        <div>
+          <h1 id="fourth-page-h1-4">The headphones of the future.</h1>
+          <p id="fifthpage-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+            repudiandae rerum libero ipsum asperiores omnis mollitia, nostrum
+            commodi placeat ea itaque modi corrupti corporis nam voluptas aut
+            reprehenderit eaque culpa.
+          </p>
+
+        </div>
+        <img src="https://i.ibb.co/37Y74kv/showcase.jpg" alt="." />
+      </div>
+    </section>
+
+    
+
+
+
+
+</>
+
         );
     }
 }
